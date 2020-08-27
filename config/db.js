430917cb4be4abdeb08087794bb34035bd5ebde3
dsoljan <soljan.domina@gmail.com>
2020-08-27 23:27:42 +0200

    Profile and user models, some routes

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true, /// is deprecated without
+      useCreateIndex: true,
+    }); //returns a promise, hence async
+
+    console.log('MongoDB Connected.');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); //exit process with a failure
+  }
+};
+
+module.exports = connectDB;
