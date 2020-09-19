6c08f63fc6fe900aa657cea03b79a861a95c85b8
dsoljan <soljan.domina@gmail.com>
2020-09-19 23:10:20 +0200

    Added info alert

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -227,7 +227,7 @@ export const deleteAccount = () => async (dispatch) => {
       dispatch({ type: CLEAR_PROFILE });
       dispatch({ type: ACCOUNT_DELETED });
 
-      dispatch(setAlert('Your account has been permanently deleted'));
+      dispatch(setAlert('Your account has been permanently deleted', 'info'));
     } catch (err) {
       dispatch({
         type: PROFILE_ERROR,
